96f0548a0ede2b3e7b5cacc8693d101297216354
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-05-30 02:09:45 +0800

    update

--- main.js
+++ main.js
@@ -12,80 +12,80 @@ const modal = document.querySelector(".modal");
 const svg = document.querySelector(".o");
 
 class IPAddressTracker{
-  #map;
-  #myIcon;
-  #mapZoomLevel = 13
-  
-  constructor(){
-    this.getPosition();
-    buttonSubmit.addEventListener("click", this.getPositionOfInputIP.bind(this));
-  }
-  
-  getPosition(){
-    console.log("getPosition called");
-    navigator.geolocation?.getCurrentPosition(
-      this.loadMap.bind(this),
-      function () {
-        alert('Could not fetch your location');
-      }
-    );
-  }
-  
-  loadMap(position){
-    console.log("loadmap called");
-    const { latitude, longitude } = position.coords;
-    const coords = [latitude, longitude];
-      &&
-    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
-    L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
-      maxZoom: 20,
-      subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
-    }).addTo(this.#map);
-  
-  this.#myIcon = L.icon({
-    iconUrl: '../images/icon-location.svg',
-    iconSize: [20, 27],
-  });
-  
-  this.getPositionOfInputIP();
-  }
-  
-  getPositionOfInputIP(){
-    console.log("getPositionOfInputIP called");
-    const ip = inputIP.value;
-    inputIP.value = "";
-    modal.style.display = "block";
-    svg.style.animationPlayState = "running";
-    const request = fetch(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=&{ip}`);
-    
-    request.then(response => response.json()).then(
-      function(data){
-        modal.style.display = "none";
-        svg.style.animationPlayState = "paused";
+    #map;
+    #myIcon;
+    #mapZoomLevel = 13
+
+    constructor(){
+        this.getPosition();
+        buttonSubmit.addEventListener("click", this.getPositionOfInputIP.bind(this));
+    }
+
+    getPosition(){
+        console.log("getPosition called");
+        navigator.geolocation?.getCurrentPosition(
+            this.loadMap.bind(this),
+            function () {
+              alert('Could not fetch your location');
+            }
+          );
+    }
+
+    loadMap(position){
+        console.log("loadmap called");
+        const { latitude, longitude } = position.coords;
+        const coords = [latitude, longitude];
+
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
+        L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+            maxZoom: 20,
+            subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+          }).addTo(this.#map);
+
+        this.#myIcon = L.icon({
+            iconUrl: '../images/icon-location.svg',
+            iconSize: [20, 27],
+        });
+
+        this.getPositionOfInputIP();
+        // this.renderMarkup(coords);
+    }
+
+    getPositionOfInputIP(){
+        console.log("getPositionOfInputIP called");
+        const ip = inputIP.value;
+        inputIP.value = "";
+        modal.style.display = "block";
+        svg.style.animationPlayState = "running";
+        const request = fetch(`https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`);
+
+        request.then(reponse => reponse.json()).then(
+            function(data){
+                modal.style.display = "none";
+                svg.style.animationPlayState = "paused";
+
+                userIP.innerHTML = data.ip;
+                userLocation.innerHTML = `${data.location.city}, ${data.location.country}`;
+                userTimezone.innerHTML = data.location.timezone;
         
-        userIP.innerHTML = data.ip;
-        userLocation.innerHTML = `${data.location.city}, ${data.location.country}`;
-        userTimezone.innerHTML = data.location.timezone;
+                if(data.isp){
+                    userServiceName.innerHTML = "isp";
+                    userServiceValue.innerHTML = data.isp;
+                }else{
+                    userServiceName.innerHTML = "usp";
+                    userServiceValue.innerHTML = data.usp;
+                } 
+                return [data.location.lat, data.location.lng];   
+            }
+        ).then((coor) => this.renderMarkup(coor)).catch((err) => console.warn(err,"🎈🎈🎈"));
         
-        if(data.isp) {
-          userServiceName.innerHTML = "isp";
-          userServiceValue.innerHTML = data.isp;
-        }else{
-          userServiceName.innerHTML = "usp";
-          userServiceValue.innerHTML = data.usp;
-        }
-        return [data.location.lat, data.location,lng];
-      }
-    ).then((coor) => this.renderMarkup(coor)).catch((err) => console.warn(err,"..."));
-    
-  }
-  
-  renderMarkup(coordinates){
-    console.log("renderMarkup called", coordinates);
-    this.#map.setView(coordinates, this.#mapZoomLevel);
-    L.marker(coordinates, {icon: this.#myIcon}).addTo(this.#map).openPopup();
-  }
-  
+    }
+
+    renderMarkup(coordinates){
+        console.log("renderMarkup called", coordinates);
+        this.#map.setView(coordinates, this.#mapZoomLevel);
+        L.marker(coordinates, {icon: this.#myIcon}).addTo(this.#map).openPopup();
+    }
 }
 
 const app = new IPAddressTracker();
