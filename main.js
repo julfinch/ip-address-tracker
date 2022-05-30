6f2a056d2d5f569454819cb3ad87fa4e4fd2c74f
Jul Danreb D Lactao <85287800+julfinch@users.noreply.github.com>
2022-05-31 02:16:44 +0800

    v0.41 added mapbox

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const errorMsgEl = document.getElementById('error-message');
 const closeBtn = document.getElementById('close-btn');
 
 const map = L.map('map').setView([0, 0], 13);
-const tiledUrl = 
+const tileUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
 
 L.tileLayer(tileUrl, {
     maxZoom: 18,
